765d7563119fc6574437e22c3a357a954ce4ef7f
GustavoRamos7 <gustavoosr94057@gmail.com>
2026-03-03 22:58:59 -0300

    computed signals

--- src/app/hello/hello.ts
+++ src/app/hello/hello.ts
@@ -1,6 +1,4 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from "@angular/router";
-import { count } from 'rxjs';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
   selector: 'app-hello',
@@ -22,4 +20,28 @@ export class Hello {
 
     protected count = signal(0);
 
+    protected doubleCount = computed(() : any =>  this.count()*2);
+
+    // getDoubleCount() {
+    //   console.log('evento do método de Contagem dupla chamado');
+    //   return this.count()*2;
+    // }
+
+    increateCounter() {
+      // count = count + 1;
+      this.count.update(value => value + 1);
+
+    }
+
+    decreaseCounter() {
+      // count = count - 1;
+      this.count.update(value => value - 1);
+
+    }
+
+    resetCounter() {
+      // count = count - 1;
+      this.count.update(value => value = 0);
+
+    }
 }
